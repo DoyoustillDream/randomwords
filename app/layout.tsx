5ab76f0a2caf0d295cff836a176cc6f6a38f6b34
konstantinos193 <96600944+konstantinos193@users.noreply.github.com>
2025-04-12 19:03:23 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,31 @@
-import type React from "react"
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import type { ReactNode } from "react";
+import "./globals.css";
+
+export const metadata = {
+  title: "Wild Token",
+  description: "Where the wild gains are",
+  generator: "v0.dev",
+  twitter: {
+    card: "summary_large_image",
+    title: "Wild Token",
+    description: "Where the wild gains are",
+    images: [
+      "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/wwr1-kKzQbejuSJ4W4Dt7f3a7nHPDz9W9WV.jpeg",
+    ],
+  },
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <title>Wild Token</title>
-        <meta name="description" content="Where the wild gains are" />
+        <link
+          rel="icon"
+          href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/wwr3-Fco9t2Mx4ue9todYl2t4Wvt3HZgI4F.jpeg"
+          type="image/jpeg"
+        />
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
-
-
-import './globals.css'
-
-export const metadata = {
-      generator: 'v0.dev'
-    };
